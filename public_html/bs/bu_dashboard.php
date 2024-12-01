164bb7ad7a7783cf4d3d013224c312d663879f80
Steve <tech-otaku@users.noreply.github.com>
2024-12-01 11:41:45 +0000

    20241201-114145

--- public_html/bs/bu_dashboard.php
+++ public_html/bs/bu_dashboard.php
@@ -22,13 +22,161 @@
             <?php include("partials/navigation.php"); ?>
         </header>
         <!-- Begin page content -->
-        <main class="flex-shrink-0">
-            <div class="container">
-                <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-                <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-                <p>Back to <a href="../examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
-            </div>
-        </main>
+        <div class="content-wrapper">
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1><?php echo $page_name; ?></h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="bu_dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="bu_manage_accounts.php">Manage Accounts</a></li>
+                                <li class="breadcrumb-item active"><?php echo $page_name; ?></li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>    <!-- /.container-fluid -->
+            </section>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- left column -->
+                        <div class="col-md-12">
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-header p-2">
+                                        <ul class="nav nav-pills">
+                                            <li class="nav-item"><a class="nav-link active" href="#update_Profile" data-toggle="tab">Update Profile</a></li>
+                                            <li class="nav-item"><a class="nav-link" href="#Change_Password" data-toggle="tab">Change Password</a></li>
+                                        </ul>
+                                    </div><!-- /.card-header -->
+                                    <div class="card-body">
+                                        <div class="tab-content">
+                                            <!-- / Update Profile -->
+                                            <div class="tab-pane active" id="update_Profile">
+                                                <form method="post" enctype="multipart/form-data" class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" name="name" required class="form-control" value="<?php //echo $row->name; ?>" id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="email" name="email" required value="<?php //echo $row->email; ?>" class="form-control" id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Contact</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" required readonly name="phone" value="<?php //echo $row->phone; ?>" id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Profile Picture</label>
+                                                        <div class="input-group col-sm-10">
+                                                            <div class="custom-file">
+                                                                <input type="file" name="profile_pic" class=" form-control custom-file-input" id="exampleInputFile">
+                                                                <label class="custom-file-label  col-form-label" for="exampleInputFile">Choose file</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Gender</label>
+                                                        <div class="col-sm-10">
+                                                            <select class="form-control" name="sex">
+                                                                <option>Male</option>
+                                                                <option>Female</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button name="update_staff_account" type="submit" class="btn btn-outline-success">Update Account</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                            <!-- /Change Password -->
+                                            <div class="tab-pane" id="Change_Password">
+                                                <form method="post" class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Old Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" class="form-control" required id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" name="password" class="form-control" required id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Confirm New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" class="form-control" required id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button type="submit" name="change_staff_password" class="btn btn-outline-success">Change Password</button>
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                            <!-- /.tab-pane -->
+                                        </div>
+                                        <!-- /.tab-content -->
+                                    </div><!-- /.card-body -->
+                                </div> <!-- card -->
+                                    <div class="card-deck">
+                                        <div class="card">
+                                            <img class="card-img-top" src="assets/download.svg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                            </div>
+                                            <div class="card-footer">
+                                            <small class="text-muted">Last updated 3 mins ago</small>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <img class="card-img-top" src="assets/download.svg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                            </div>
+                                            <div class="card-footer">
+                                            <small class="text-muted">Last updated 3 mins ago</small>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <img class="card-img-top" src="assets/download.svg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                            </div>
+                                            <div class="card-footer">
+                                            <small class="text-muted">Last updated 3 mins ago</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+</div>
     <!-- Common Footer -->
         <?php include("partials/footer.php"); ?>
     <!-- jQuery -->
